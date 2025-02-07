04dd361cb02b1b7f388ee5f74702814bd99f0f43
Dwi Wjiaya <dev.dwiwijaya@gmail.com>
2025-02-07 10:22:16 +0700

    fix

--- service-worker.js
+++ service-worker.js
@@ -1,9 +1,18 @@
 self.addEventListener("notificationclick", function (event) {
     event.notification.close();
 
-    if (event.action === "play") {
-        clients.openWindow("/play");
-    } else if (event.action === "dismiss") {
-        console.log("Pengguna menutup notifikasi.");
-    }
+    event.waitUntil(
+        clients.matchAll({ type: "window", includeUncontrolled: true }).then(clientList => {
+            if (event.action === "play") {
+                if (clientList.length > 0) {
+                    clientList[0].focus();
+                    clientList[0].navigate("/play");
+                } else {
+                    clients.openWindow("/play");
+                }
+            } else {
+                console.log("Pengguna menutup notifikasi.");
+            }
+        })
+    );
 });
